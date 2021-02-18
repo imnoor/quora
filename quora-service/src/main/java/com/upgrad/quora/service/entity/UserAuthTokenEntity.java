4e0531eaff3922dc267187be159a9161660780cc
Yaseen <mhdyaz@outlook.com>
2021-02-18 23:52:12 +0300

    Removed schema name and updated with @OnDelete to cascade delete of access tokens.

--- quora-service/src/main/java/com/upgrad/quora/service/entity/UserAuthTokenEntity.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UserAuthTokenEntity.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.time.ZonedDateTime;
 @Entity
-@Table(name = "user_auth", schema = "quora")
+@Table(name = "user_auth")
 @NamedQueries({
         @NamedQuery(name = "userAuthTokenByAccessToken", query = "select ut from UserAuthTokenEntity ut where ut.accessToken = :accessToken ")
 })
